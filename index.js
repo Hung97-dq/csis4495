d42de92c1679dd2339c8bb41702196515b914cda
Hung Dinh <hung.dq1997hn@gmail.com>
2023-04-13 02:33:23 -0700

    make it better

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ app.use('/posts', postRoutes);
 app.use('/user', useRoutes);
 const CONNECTION_URL = 'mongodb+srv://hung:!23456@cluster0.yptzo9p.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
-app.get('/', (req,res) =>{
-    res.send('APP IS RUNNING');
-})
+
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology: true })
     .then(()=>app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) =>console.log(error.message));
